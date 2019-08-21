49ee2e67c3d2f0781f688ccc2c523ddbe2bb6d60
BohriumKwong <bohrium.kwong@gmail.com>
2019-08-21 13:33:39 +0800

    对early_stopping显示信息部分进行小调整。

--- utils/train.py
+++ utils/train.py
@@ -232,7 +232,7 @@ class Epoch:
                         early_stop_flag = early_stop_flag + 1   
                         # Let early_stop_flag itself adds by 1, which is used by early_stopping 
                     print()
-            elif ~stop_print:
+            elif not stop_print:
                 print("Epoch early stop: {}/{}".format(early_stop_flag,epoch))
                 stop_print = True
 
